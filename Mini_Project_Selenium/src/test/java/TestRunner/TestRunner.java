138374d24c7b7d80319de7f74489566636e2973d
99002477 <71481753+99002477@users.noreply.github.com>
2020-11-08 22:41:29 +0530

    Add files via upload

---
+++ Mini_Project_Selenium/src/test/java/TestRunner/TestRunner.java
@@ -0,0 +1,19 @@
+package TestRunner;
+
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class) 
+
+	@CucumberOptions(
+	        features = "src/test/resources/feature",
+	        glue= {"specDefinitions"},
+	        plugin = { "html:target/Report-html"},
+	        monochrome = true
+	        )
+
+	public class TestRunner {
+}
